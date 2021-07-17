b6e62fd8adf82639d114df1a8b483b7f186d55d1
Angga Prahmana <developer.angga@gmail.com>
2021-07-17 12:02:29 +0700

    cloud mongodb

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : "mongodb://localhost:27017/angga"
+    // url : "mongodb://localhost:27017/angga"
+    url : "mongodb+srv://devAnggaa:12345@cluster0.spsoq.mongodb.net/angga"
 }
