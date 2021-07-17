292a964392c5822578118ebbc74d3dc47b980ee7
Angga Prahmana <developer.angga@gmail.com>
2021-07-17 12:19:14 +0700

    testing port

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get(`/`, (req, res) => {
  
 require('./app/routes/post.routes')(app)
 
-const PORT = process.env.PORT || 8000
+const PORT = 8000
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
